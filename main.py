f6b3fc1a002af97186da0bc4a9f025e3abb4af95
Ryan Tse <rytse@mbhs.edu>
2017-06-13 18:44:09 -0400

    Set up write to motor what is read from file

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+import util;
 import mp_io as mio;
+import motor-shield.PiMotor as pim
 
 def main:
+    left_m = pim.Motor("MOTOR1", 1);
+    right_m = pim.Motor("MOTOR2", 1);
+    arm_m = pim.Motor("MOTOR3", 1);
+    turret_m = pim.Motor("MOTOR4", 1);
+
     while True:
         in_data = mio.readfile();
-        upate_hw();
 
-def update_hw():
-    pass;
+        util_setmotor(left_m, in_data["left_m"];
+        util_setmotor(right_m, in_data["right_m"];
 
 if __name__ == "__main__":
     main()
